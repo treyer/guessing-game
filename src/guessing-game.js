75b6886f6d09d6191d71cfe7705a076adf658a5c
treyer <nemereno_by@mail.ru>
2021-09-04 11:01:26 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.start = null;
+        this.end = null;
+        this.guessPosition = null;
+    }
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        if (this.start <= this.end){
+            this.guessPosition = Math.round((this.start + this.end) / 2);
+        }
+        return this.guessPosition;
     }
 
     lower() {
-
+        this.end = this.guessPosition;
     }
 
     greater() {
-
+        this.start = this.guessPosition;
     }
 }
 
